859a3764c34e846e73c1eaa1e648c51c978b3439
prateekdub <prateekd14@gmail.com>
2025-02-27 23:42:06 +0530

    if task is small

--- src/TaskBoard/column/Column.js
+++ src/TaskBoard/column/Column.js
@@ -1,31 +1,39 @@
 import React from "react";
 import TaskCard from "../taskCard/TaskCard";
+import { STATUS } from "../config";
 
-export function Column({ status, tasks, heading = '', ticketTitle, ticketId, addSubtask, updateTaskTitle, ticketStatus }) {
+export function Column({ status, tasks, heading = '', ticketTitle, ticketId, addSubtask,
+  updateTaskTitle, ticketStatus }) {
 
-  if (status == "mainStory") {
+  if (heading) {
+    return <div className="column">{heading}</div>
+  }else if ((!tasks || tasks.length == 0) && ticketStatus == status) {
     const obj = {
       id: ticketId,
       title: ticketTitle
     }
     return (
       <div className="column">
-        <TaskCard status={ticketStatus} key={obj.id} task={obj} showAdd={true} addSubtask={addSubtask} updateTaskTitle={updateTaskTitle} data={{parent: true}}/>
+        <TaskCard status={ticketStatus} key={obj.id} task={obj} showAdd={true} addSubtask={addSubtask} updateTaskTitle={updateTaskTitle} data={{ parent: true }} />
+      </div>
+    )
+  }else if (status == STATUS.MAIN_STORY) {
+    const obj = {
+      id: ticketId,
+      title: ticketTitle
+    }
+    return (
+      <div className="column">
+        <TaskCard status={ticketStatus} key={obj.id} task={obj} showAdd={true} addSubtask={addSubtask} updateTaskTitle={updateTaskTitle} data={{ parent: true }} />
       </div>
     )
   }
-  if ((!tasks || tasks.length == 0)) {
-    return <div className="column">{heading}</div>;
-  }
+  
   return (
     <div className="column">
-      {/* {
-        ['todo', 'inProgress', 'done'].includes(ticketStatus) && !tasks?.length && (
-          <TaskCard key={ticketTitle} task={[]} status={ticketStatus} addSubtask={addSubtask} updateTaskTitle={updateTaskTitle}/>
-        )
-      } */}
+
       {tasks?.length > 0 && tasks.map((task) => (
-        <TaskCard key={task.id} task={task} status={task.status} addSubtask={addSubtask} updateTaskTitle={updateTaskTitle}/>
+        <TaskCard key={task.id} task={task} status={task.status} addSubtask={addSubtask} updateTaskTitle={updateTaskTitle} />
       ))}
     </div>
   );
